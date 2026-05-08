b8a3581fdc2b1d099236c4298be3027a7d259d2b
Harshit Saraswat <167835509+Rig135@users.noreply.github.com>
2026-05-08 15:10:28 +0530

    loggerFunction

---
+++ loggingMiddleware/loggingFunction.js
@@ -0,0 +1,22 @@
+async function log(stack,level,packageName,message){
+    try{
+        const response = await fetch("http://4.224.186.213/evaluation-service/logs", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                stack,
+                level,
+                package: packageName,
+                message,
+            }),
+        });
+
+        return await response.json();
+
+    }
+    catch(error){
+        console.log("loggin failed",error.message);
+    }
+}
